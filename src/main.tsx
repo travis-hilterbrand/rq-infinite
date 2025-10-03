131d02b8cdebd87010f2468d0caddf020b47a70e
Travis Hilterbrand <travis_hilterbrand@yahoo.com>
2025-10-02 19:59:58 -0700

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+async function enableMocking() {
+  const { worker } = await import("./mocks/browser");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
---
+++ src/mocks/browser.ts
@@ -0,0 +1,37 @@
+import { http, HttpResponse } from "msw";
+import { setupWorker } from "msw/browser";
+
+const PAGE_SIZE = 10;
+const TOTAL_ITEMS = 50;
+
+type Item = {
+  id: number;
+  name: string;
+};
+const items: Item[] = Array.from({ length: TOTAL_ITEMS }, (_, i) => ({
+  id: i + 1,
+  name: `Item ${i + 1}`,
+}));
+
+const getParamInt = (param: string | null): number => {
+  if (param) {
+    return parseInt(param);
+  }
+  return 0;
+};
+
+export const worker = setupWorker(
+  http.get("/api/items", async ({ request }) => {
+    const url = new URL(request.url);
+    const page: number = getParamInt(url.searchParams.get("page"));
+    const start = (page - 1) * PAGE_SIZE;
+    const end = start + PAGE_SIZE;
+    const pagedItems = items.slice(start, end);
+    return HttpResponse.json({
+      items: pagedItems,
+      total: TOTAL_ITEMS,
+      page,
+      pageSize: PAGE_SIZE,
+    });
+  })
+);
